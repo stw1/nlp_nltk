1d9d2525a93492b9033c7f154edba78c5cc0e3ce
Stephen Wong <stwong1@gmail.com>
2016-09-28 04:41:57 +0000

    DOK with sentence code

--- dok/dok.py
+++ dok/dok.py
@@ -1,7 +1,7 @@
 import nltk
 import csv
 from textblob.classifiers import NaiveBayesClassifier
-from textblob import TextBlob
+from nltk.tokenize import sent_tokenize
 
 
 train = []
@@ -21,38 +21,34 @@ test = [
 cl = NaiveBayesClassifier(train)
 
 # Classify a TextBlob
-# example_sentence = "Can you really experience anything objectively.  Formulate, critique, then investigate your answer?"
+# example_sentence = "Can you really experience anything objectively."
+# example_sentence2 = "Gather information to develop alternative explanations for"
 example_sentence = "How would you describe ants?  Gather information to develop alternative explanations for?"
-#example_sentence = "Gather information to develop alternative explanations for"
 
-# print(cl.classify(example_sentence))
+# This function prints the probability
+def print_probability(sentence):
+	print(sentence)
+	prob_dist = cl.prob_classify(sentence)
+	print("Probability of Level 1: ", (round(prob_dist.prob("1"), 2) * 100), "%")
+	print("Probability of Level 2: ", (round(prob_dist.prob("2"), 2) * 100), "%")
+	print("Probability of Level 3: ", (round(prob_dist.prob("3"), 2) * 100), "%")
+	print("Probability of Level 4: ", (round(prob_dist.prob("4"), 2) * 100), "%")
 
-blob = TextBlob(example_sentence, classifier=cl)
+
+	if (cl.classify(sentence) == "1"):
+		print("Level One: Recall")
+	elif (cl.classify(sentence) == "2"):
+		print("Level Two: Skill/Concept")
+	elif (cl.classify(sentence) == "3"):
+		print("Level Three: Strategic Thinking")
+	else:
+		print("Level Four: Extended Thinking")
 
 
-
-for sentence in blob.sentences:
-    print("Sentence: ", sentence)
-
-    prob_dist = cl.prob_classify(sentence)
-
-    print("Probability of Level 1 : ", round(prob_dist.prob("1"), 2))
-    print("Probability of Level 2: ", round(prob_dist.prob("2"), 2))
-    print("Probability of Level 3: ", round(prob_dist.prob("3"), 2))
-    print("Probability of Level 4: ", round(prob_dist.prob("4"), 2))
-    
-
-    if (sentence.classify() == "1"):
-        print("Level One: Recall")
-    elif (sentence.classify() == "2"):
-        print("Level Two: Skill/Concept")
-    elif (sentence.classify() == "3"):
-        print("Level Three: Strategic Thinking")
-    else:
-        print("Level Four: Extended Thinking")
-
-    print("")
-
+# Code to analysis each sentence.  One at a time.
+sentence = sent_tokenize(example_sentence)
+for i in sentence:
+	print_probability(i)
 
     
 
